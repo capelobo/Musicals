7bc85ca801376ee4f2dce77c07d53179b8b4ad5e
Fabio Bruni <fabiobruni@gmail.com>
2020-09-23 19:37:57 -0300

    coloquei a pagina inicial para redirecionar para o index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :instruments do
     resources :orders, only: [:new, :create]
   end
+  root to: 'instruments#index'
 end
