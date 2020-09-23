bf6aa2f09c37515c71e39eb9a8b0bee15146c6df
Adilson Carvalho <adilson.santana.de.carvalho@gmail.com>
2020-09-23 14:52:57 -0300

    routes implemented

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_210611) do
+ActiveRecord::Schema.define(version: 2020_09_23_142201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,15 @@ ActiveRecord::Schema.define(version: 2020_09_22_210611) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_instruments_on_user_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "instrument_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["instrument_id"], name: "index_orders_on_instrument_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "transactions", force: :cascade do |t|
@@ -47,6 +56,8 @@ ActiveRecord::Schema.define(version: 2020_09_22_210611) do
   end
 
   add_foreign_key "instruments", "users"
+  add_foreign_key "orders", "instruments"
+  add_foreign_key "orders", "users"
   add_foreign_key "transactions", "instruments"
   add_foreign_key "transactions", "users"
 end
