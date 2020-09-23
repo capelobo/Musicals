5be8a3dc2fa0045047d59fd7abf97600f97999bc
Adilson Carvalho <adilson.santana.de.carvalho@gmail.com>
2020-09-23 16:22:19 -0300

    edit and update implemented

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -3,4 +3,28 @@ class InstrumentsController < ApplicationController
   def index
     @instruments = Instrument.all
   end
+
+  def edit
+    set_instrument
+  end
+
+  def update
+    set_instrument
+    @instrument.update(instrument_params)
+      redirect_to instrument_path(@instrument)
+  end
+
+
+
+end
+
+
+private
+
+def set_instrument
+  @instrument = Instrument.find(params[:id])
+end
+
+def instrument_params
+  params.require(:instrument).permit(:name, :description, :price)
 end
