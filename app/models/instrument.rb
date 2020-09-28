fd11436f84cbd66ff9cc082757ea44b63b7ae0a6
Marcos Flores <marcoschines@gmail.com>
2020-09-28 00:59:46 -0300

    mais alguns detalhes

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -3,6 +3,10 @@ class Instrument < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_one :order
 
+  validates :name, length: { in: 3..18 }, presence: true
+  validates :photo, presence: true
+  validates :price, presence: true
+
   include PgSearch::Model
   pg_search_scope :search_by_name_and_description,
                   against: [:name, :description],
